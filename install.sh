cdf86bbf763fbf0968c00ebe1c2083d8f80894c1
Jakob Stechow <jakob.stechow@pm.me>
2025-09-10 00:49:10 +0200

    fix: drop 1password

--- install.sh
+++ install.sh
@@ -115,16 +115,6 @@ fi
 
 rustup component add rust-analyzer
 
-if ! command -v 1password >/dev/null 2>&1
-then
-	echo "Installing 1password"
-	 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
-	 sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-	sudo dnf install 1password
-else
-	echo "1password already installed, skipping"
-fi
-
 # font
 mkdir -p ~/.local/share/fonts
 if [ ! -f /home/invertedecho/.local/share/fonts/JetBrainsMonoNLNerdFontMono-Regular.ttf ]; then
